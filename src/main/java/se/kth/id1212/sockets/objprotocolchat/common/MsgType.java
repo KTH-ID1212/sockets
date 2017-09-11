457f43ef3c841b1831f7bb2e4e489d11c1ae435a
Leif Lindbäck <leifl@kth.se>
2017-09-11 16:52:30 +0200

    Refactorings

--- src/main/java/se/kth/id1212/sockets/objprotocolchat/common/MsgType.java
+++ src/main/java/se/kth/id1212/sockets/objprotocolchat/common/MsgType.java
@@ -41,7 +41,7 @@ public enum MsgType {
      */
     BROADCAST,
     /**
-     * Client is about to close, all server recourses related to the sending client shod be
+     * Client is about to close, all server recourses related to the sending client should be
      * released.
      */
     DISCONNECT
